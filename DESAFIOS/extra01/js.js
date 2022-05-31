4be7c0f33c9066d2dd4df36369ac0724a2b64bd1
João Alves <106602806+Aalvees@users.noreply.github.com>
2022-05-31 10:58:55 -0300

    Initial commit

---
+++ DESAFIOS/extra01/js.js
@@ -0,0 +1,4 @@
+var salario = window.prompt("Digite seu salário bruto de aprendiz")
+document.write(`<h1> INSS desconta R$${salario*0.075} do seu salário de R$${salario}. Portanto seu salário liquido é de R$${salario*0.915}! <br>`)
+document.write(`Você custa cerca de R$${salario*2.5} Para a sua empresa! <br>`)
+document.write(`Achou caro? <br> Ou será que você sente que está ganhando pouca grana?`)
